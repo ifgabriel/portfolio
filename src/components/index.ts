be47b09f836f7f7d6cadaa4d5a2397cbdaa2a3d6
ifgabriel <gabrielmirandap.ti@gmail.com>
2023-06-12 19:52:14 -0300

    fix(portfolio): fix component export file

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,3 @@ export { default as Contact } from './Contact'
 export { default as Experiences } from './Experiences'
 export { default as Projects } from './Projects'
 export { default as Skills } from './Skills'
-
