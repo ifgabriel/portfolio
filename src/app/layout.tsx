165ce840f3939ec47512aad9a2904badba187c5c
ifgabriel <gabrielmirandap.ti@gmail.com>
2023-06-12 18:18:51 -0300

    feat(portfolio): create skills section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
 export const metadata = {
-  title: 'Gabriel Miranda',
+  title: "Gabriel Miranda' Portfolio",
 }
 
 interface LayoutProps {
@@ -14,8 +14,13 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => (
   <html lang="pt-br">
-    <body className={`${inter.variable} bg-slate-50-900 font-sans`}>
-      <main className="container">{children}</main>
+    <body
+      className={`${inter.variable} bg-slate-50-900 bg-primary-50 font-sans text-zinc-50`}
+    >
+      <main className="container mx-auto flex flex-col gap-16 overflow-hidden border-l border-l-primary-200 px-6 py-32 lg:px-52">
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+        {children}
+      </main>
     </body>
   </html>
 )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-const Page = () => <h1>Page</h1>
+'use client'
+import { About, Contact, Experiences, Skills } from '@/components'
+import useWindowSize from '@/utils/useWindowSize'
+
+const Page = () => {
+  const windowSize = useWindowSize()
+
+  return (
+    <>
+      <About />
+      <Experiences />
+      {windowSize > 1000 && <Skills />}
+      <Contact />
+    </>
+  )
+}
 
 export default Page
