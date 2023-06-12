d6271fd84472b0090fbb14f9a4087e02b485f750
ifgabriel <gabrielmirandap.ti@gmail.com>
2023-06-12 19:41:37 -0300

    feat(portfolio): add sharp

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,9 +3,9 @@ const Contact = () => (
     <div className="mb-4 h-1 w-32 bg-primary-100" />
     <h2 className="text-5xl font-semibold">Contact</h2>
     <p className="text-l mt-24 max-w-3xl">
-      I'm currently looking to join a team that seeks to improve people's lives
+      I’m currently looking to join a team that seeks to improve people’s lives
       with web solutions, through effective and accessible solutions. Do you
-      have a project in mind? let's connect.
+      have a project in mind? let’s connect.
     </p>
   </section>
 )
