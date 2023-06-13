ff5842648930f18d99c848c33dcfb673f02393d4
ifgabriel <gabrielmirandap.ti@gmail.com>
2023-06-13 10:58:33 -0300

    feat(portfolio): add contact information

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,3 +1,5 @@
+import { GithubIcon, LinkedinIcon, MailIcon } from 'lucide-react'
+
 const Contact = () => (
   <section className="container relative mx-auto flex  flex-col py-14 lg:px-52">
     <div className="mb-4 h-1 w-32 bg-primary-100" />
@@ -7,6 +9,21 @@ const Contact = () => (
       with web solutions, through effective and accessible solutions. Do you
       have a project in mind? let’s connect.
     </p>
+    <div className="mt-10 flex gap-4">
+      <a href="https://github.com/ifgabriel" target="_blank" rel="noreferrer">
+        <GithubIcon className="text-primary-100" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/gabriel-miranda-1589a51b0/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <LinkedinIcon className="text-primary-100" />
+      </a>
+      <a href="mailto:gabrielmp@unipam.edu.br" target="_blank" rel="noreferrer">
+        <MailIcon className="text-primary-100" />
+      </a>
+    </div>
   </section>
 )
 
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,50 +1,91 @@
 import project1 from '@/assets/project-1.png'
 import project2 from '@/assets/project-2.png'
+import project3 from '@/assets/project-3.png'
 import Image, { StaticImageData } from 'next/image'
 
 interface ProjectType {
   title: string
   image: StaticImageData
   description: string
+  link: string
+  technologies: string[]
 }
 
 const projects: ProjectType[] = [
   {
     title: 'Spacetime',
     image: project1,
+    link: 'https://github.com/ifgabriel/spacetime',
     description: `
-      Memory recall application, where the user can add texts, photos and videos of important 
-      events in his life to a timeline, organized by month and year.
+      Memory recovery application, where the user can add texts, photos and videos of important 
+      events in his life in a timeline, organized by month and year.
+
+      This project was developed together with the NLW event promoted by rockeseat.
     `,
+    technologies: ['React', 'Next', 'Typescript', 'Tailwind', 'Node'],
   },
   {
     title: 'Kanban',
     image: project2,
-    description: `Task management application, where the user can add tasks, with title, description,
-    status. In this way, it is possible to manage tasks based on status and priority.
+    link: 'https://github.com/ifgabriel/kanban',
+    description: `
+      Task management application, where user can add tasks, with title, description,
+      status. In this way, you can manage tasks based on status and priority.
+
+      This project was created to be used as a showcase of my knowledge in mirageJS and API consumption.
     `,
+    technologies: ['React', 'Next', 'Typescript', 'Tailwind', 'MirageJS'],
+  },
+  {
+    title: 'Utils',
+    image: project3,
+    link: 'https://github.com/ifgabriel/utils',
+    description: `
+      In this project, it is possible to visualize common functions in a developer's 
+      everyday life. The repository is made up of some of these functions, just code, there is no UI.
+
+      It is a place of presentation of programming logic and unit tests.
+    `,
+    technologies: ['React', 'Jest', 'Typescript', 'React Testing Library'],
   },
 ]
 
-const Project = ({ title, image, description }: ProjectType) => (
-  <li className="relative flex flex-row flex-wrap justify-between gap-8">
-    <div>
-      <span className="text-base text-primary-100">Featured Project</span>
-      <h3 className="mb-8 text-2xl font-semibold">{title}</h3>
-      <div className="left-0 max-w-xl rounded-md bg-white/30 p-6 backdrop-blur-md md:absolute">
-        <p className="text-sm">{description}</p>
+const Project = ({
+  title,
+  image,
+  description,
+  technologies,
+  link,
+}: ProjectType) => (
+  <li>
+    <a
+      href={link}
+      target="_blank"
+      rel="noreferrer"
+      className="relative flex flex-row flex-wrap justify-between gap-8"
+    >
+      <div>
+        <span className="text-base text-primary-100">Featured Project</span>
+        <h3 className="mb-8 text-2xl font-semibold">{title}</h3>
+        <div className="left-0 max-w-xl rounded-md bg-white/30 p-6 backdrop-blur-md md:absolute">
+          <p className="text-sm">{description}</p>
+          <p className="mt-4 text-sm">
+            It was created using{' '}
+            <span className=" text-primary-100">{technologies.join(', ')}</span>
+          </p>
+        </div>
       </div>
-    </div>
-    <div className="w-fit overflow-hidden rounded-xl bg-primary-100 pt-5 md:pl-10">
-      <Image
-        className="rounded-md object-cover"
-        width={568}
-        height={354}
-        alt={title}
-        src={image}
-        aria-hidden
-      />
-    </div>
+      <div className="w-fit rounded-xl bg-primary-100 pt-5 md:pl-10">
+        <Image
+          className="rounded-md object-cover"
+          width={568}
+          height={354}
+          alt={title}
+          src={image}
+          aria-hidden
+        />
+      </div>
+    </a>
   </li>
 )
 
